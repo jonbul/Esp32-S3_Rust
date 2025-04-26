ffbe7fefcf04a47d648ae17b54da6f4d6a79962e
Jon Ander <jonander.echeverria@gmail.com>
2025-04-26 16:17:37 +0200

    some pwm effects

--- a02_led_blink_pwm/src/main.rs
+++ a02_led_blink_pwm/src/main.rs
@@ -13,26 +13,92 @@ fn main() {
     esp_idf_svc::log::EspLogger::initialize_default();
 
     log::info!("Hello, world!");
+    
 
     let peripherals = Peripherals::take().unwrap();
-    let timer_driver = LedcTimerDriver::new(peripherals.ledc.timer0, &TimerConfig::default().frequency(25.kHz().into())).unwrap();
-    let mut driver = LedcDriver::new(peripherals.ledc.channel0, timer_driver, peripherals.pins.gpio1).unwrap();
+    let timer_driver_r = LedcTimerDriver::new(peripherals.ledc.timer0, &TimerConfig::default().frequency(25.kHz().into())).unwrap();
+    let timer_driver_g = LedcTimerDriver::new(peripherals.ledc.timer1, &TimerConfig::default().frequency(25.kHz().into())).unwrap();
+    let timer_driver_b = LedcTimerDriver::new(peripherals.ledc.timer2, &TimerConfig::default().frequency(25.kHz().into())).unwrap();
+    
+    let r_driver = LedcDriver::new(peripherals.ledc.channel0, timer_driver_r, peripherals.pins.gpio4).unwrap();
+    let g_driver = LedcDriver::new(peripherals.ledc.channel1, timer_driver_g, peripherals.pins.gpio1).unwrap();
+    let b_driver = LedcDriver::new(peripherals.ledc.channel2, timer_driver_b, peripherals.pins.gpio3).unwrap();
 
-    let max_duty = driver.get_max_duty();
-    let mut duty:u32 = 0;
-    log::info!("max duty {}", max_duty);
-
-    let mut up = true;
+    let mut r_pwm_driver = PwmDriver::new(r_driver, 0);
+    let mut g_pwm_driver = PwmDriver::new(g_driver, 0);
+    let mut b_pwm_driver = PwmDriver::new(b_driver, 0);
+    //let mut g_pwm_driver = PwmDriver::new(g_driver, 64);
+    //let mut b_pwm_driver = PwmDriver::new(b_driver, 128);
 
     loop {
-        //log::info!("duty {}", duty);
-        driver.set_duty(duty);
+        r_pwm_driver.breath();
+        g_pwm_driver.breath();
+        b_pwm_driver.breath();
         
-        duty = if up { duty + 1 } else { duty - 1 };
-        if duty >= max_duty || duty <= 0 {
-            up = !up;
-        }
         delay::FreeRtos::delay_ms(5);
+    }
 
+    /*let mut r_count:u8 = 0;
+    let mut g_count:u8 = 0;
+    let mut b_count:u8 = 0;
+    loop {
+        log::info!("r {} g {} b {}", r_count, g_count, b_count);
+
+        r_pwm_driver.set(r_count.into());
+        g_pwm_driver.set(g_count.into());
+        b_pwm_driver.set(b_count.into());
+
+        if (r_count == 255) {
+            r_count = 0;
+            
+            if g_count == 255 {
+                g_count = 0;
+
+                if b_count == 255 {
+                    b_count = 0;
+                } else {
+                    b_count += 1;
+                }
+            } else {
+                g_count += 1;
+            }
+            
+        } else {
+            r_count += 1;
+        }
+
+        //delay::FreeRtos::delay_ms(5);
+    }*/
+}
+
+struct PwmDriver<'a> {
+    driver: LedcDriver<'a>,
+    duty: u32,
+    up: bool,
+    max_duty: u32,
+}
+impl<'a> PwmDriver<'a> {
+    fn new(driver: LedcDriver<'a>, duty: u32) -> Self {
+        let max_duty = driver.get_max_duty();
+        Self {
+            driver,
+            duty,
+            up: true,
+            max_duty,
+        }
+    }
+
+    fn breath(&mut self) {
+        let _ = self.driver.set_duty(self.duty);
+
+        self.duty = if self.up { self.duty + 1 } else { self.duty - 1 };
+
+        if self.duty >= self.max_duty || self.duty == 0 {
+            self.up = !self.up;
+        }
+    }
+
+    fn set(&mut self, duty: u32) {
+        let _ = self.driver.set_duty(duty);
     }
 }
